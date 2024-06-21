ed2cac508fc24bd6ce8a950eee809508a2927c18
nifnip22 <proxigaming742@gmail.com>
2024-06-21 16:43:30 +0800

    Routing in Next.js

---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function About() {
+    return (
+        <h1>About Me</h1>
+    )
+}
+
+// Masukkan '/about' di URL localhost:3000 untuk melihat halaman ini
---
+++ src/app/profile/page.tsx
@@ -0,0 +1,7 @@
+export default function Profile() {
+    return (
+        <h1>My Profile</h1>
+    )
+}
+
+// Masukkan '/profile' di URL localhost:3000 untuk melihat halaman ini
