17e9eb8242951f17257b32497b7f7fad3c66f36b
nifnip22 <proxigaming742@gmail.com>
2024-06-22 19:12:45 +0800

    How to make Active Links in Next.js

--- src/app/(auth)/(with-auth-layout)/layout.tsx
+++ src/app/(auth)/(with-auth-layout)/layout.tsx
@@ -2,13 +2,35 @@
     Route Group bisa digunakan oleh layout untuk menerapkan tata letak secara selektif pada segmen tertentu dan
     meninggalkan yang lainnya tidak berubah. Contohnya kita ingin membuat sebuah layout untuk halaman register dan
     login namun tidak pada halaman forgot password.
-*/ 
+*/
+
+// Active links example
+'use client';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import "../../styles.css"
+
+const navLinks = [
+	{ name: 'Register', href: '/register' },
+	{ name: 'Login', href: '/login' },
+];
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
-    return (
-        <>
-            <h2>Inner Layout</h2>
-            {children}
-        </>
-    )
+
+	const pathname = usePathname();
+
+	return (
+		<>
+			{navLinks.map((link) => {
+				const isActive = pathname.startsWith(link.href);
+
+				return (
+					<Link key={link.name} href={link.href} className={isActive ? 'font-bold mr-4' : 'text-blue-500 mr-4'}>
+						{link.name}
+					</Link>
+				);
+			})}
+			{children}
+		</>
+	);
 }
