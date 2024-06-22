7bf6a135ff5585f7ff8db069e16c0df886ff30a1
nifnip22 <proxigaming742@gmail.com>
2024-06-22 17:47:17 +0800

    Routing and Dynamic Metadata

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,12 @@
+/*
+    Metadata yang ada pada layout.tsx di folder app merupakan metadata yang akan di terapkan di seluruh route, namun
+    kita bisa membuat metadata sendiri untuk diterapkan pada route tertentu yang kita buat. 
+*/ 
+
+export const metadata = {
+    title: 'About Next.js'
+}
+
 export default function About() {
     return (
         <h1>About Me</h1>
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -3,7 +3,23 @@
     ataupun fetch product ID dari API jika nantinya menggunakan API.
 */
 
-export default function ProductDetails({ params }: { params: { productId: number } }) {
+import { Metadata } from 'next';
+
+// Cara membuat Dynamic Metadata untuk membuat title mengambil product ID
+type Props = {
+	params: {
+		productId: string;
+	};
+};
+
+export const generateMetadata = async ({ params }: Props): Promise<Metadata> => {
+	const title = await new Promise((resolve) => setTimeout(() => resolve(`iPhone ${params.productId}`), 100));
+	return {
+		title: `Product ${title}`,
+	};
+};
+
+export default function ProductDetails({ params }: Props) {
 	return <h1>Details About This Product {params.productId}</h1>;
 }
 
