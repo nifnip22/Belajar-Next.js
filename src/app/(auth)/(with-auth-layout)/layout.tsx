91663204d8fc86eef5095e2bf99b825470ce7d1c
nifnip22 <proxigaming742@gmail.com>
2024-06-30 17:26:25 +0800

    Next.js Templates Understanding (Not Really Understand)

--- src/app/(auth)/(with-auth-layout)/layout.tsx
+++ src/app/(auth)/(with-auth-layout)/layout.tsx
@@ -8,7 +8,8 @@
 'use client';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import "../../styles.css"
+import '../../styles.css';
+import { useState } from 'react';
 
 const navLinks = [
 	{ name: 'Register', href: '/register' },
@@ -16,11 +17,17 @@ const navLinks = [
 ];
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
-
 	const pathname = usePathname();
+
+	// Input templating
+	const [input, setInput] = useState('');
 
 	return (
 		<>
+			<div>
+				<input value={input} onChange={(e) => setInput(e.target.value)} />
+				{/* Jika sudah mengisi input, coba redirect ke halaman login */}
+			</div>
 			{navLinks.map((link) => {
 				const isActive = pathname.startsWith(link.href);
 
